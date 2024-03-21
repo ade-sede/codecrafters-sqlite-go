f9bdf31d1d293c3999866cbc357c38edbba85ca2
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-03-21 15:21:13 +0100

    feat: SELECT COUNT(*)

--- app/main.go
+++ app/main.go
@@ -95,7 +95,84 @@ func main() {
 		fmt.Println(allTables)
 
 	default:
-		fmt.Println("Unknown command", command)
-		os.Exit(1)
+		if len(os.Args) <= 2 {
+			log.Fatal("No command specified")
+		}
+
+		sqlQuery := os.Args[2]
+		stmt, err := sqlparser.Parse(sqlQuery)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch stmt := stmt.(type) {
+		case *sqlparser.Select:
+			fromExpr := sqlparser.GetTableName(stmt.From[0].(*sqlparser.AliasedTableExpr).Expr.(sqlparser.TableName))
+
+			if fromExpr.IsEmpty() {
+				log.Fatal("No from table specified in select query")
+			}
+
+			tableName := fromExpr.String()
+			var tableRootPageNumber int
+
+			for _, records := range allSchemaRecords {
+				if string(records[2].payload) == tableName {
+					tableRootPageNumber = int(records[3].payload[0])
+				}
+			}
+
+			tableRootPage, err := database.getPage(tableRootPageNumber - 1)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			tableCells, err := tableRootPage.cells()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			allTablePageRecords := make([][]*Record, 0)
+
+			for _, cell := range tableCells {
+				records, err := decodePayload(cell.payloadHeader, cell.payloadBody)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				allTablePageRecords = append(allTablePageRecords, records)
+			}
+
+			for _, selectExpr := range stmt.SelectExprs {
+				switch selectExpr := selectExpr.(type) {
+				case *sqlparser.StarExpr:
+					fmt.Println("StarExpr")
+				case *sqlparser.AliasedExpr:
+					if funcExpr, ok := selectExpr.Expr.(*sqlparser.FuncExpr); ok {
+						funcName := funcExpr.Name.String()
+
+						if strings.ToUpper(funcName) == "COUNT" {
+							if _, ok := funcExpr.Exprs[0].(*sqlparser.StarExpr); ok {
+								fmt.Println(len(allTablePageRecords))
+							}
+						} else {
+							fmt.Println("Unsupported function", funcName)
+						}
+					}
+				default:
+					fmt.Println("Neither StarExpr nor AliasedExpr")
+				}
+			}
+
+		default:
+			fmt.Println("Unsupported query", sqlQuery)
+			os.Exit(1)
+		}
+
+		os.Exit(0)
 	}
 }
